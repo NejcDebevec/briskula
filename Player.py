1b315e28f2ecb9ae33cc735d21a5c7a137840dde
Nejc Debevec <nd8380@student.uni-lj.si>
2018-05-21 16:38:41 +0200

    player updated

--- Player.py
+++ Player.py
@@ -1,11 +1,23 @@
 import Deck
 
 class Player:
-    def __init__(self):
+    def __init__(self, deck):
+        self.deck = deck
         self.cards_collected = []
         self.score = 0
-        self.current_cards =[]
-
+        self.current_cards = []
 
     def pickUp_card(self):
-        self.current_cards.append()
+        self.current_cards.append("")
+
+    def add_to_loot(self, card1, card2):
+        self.cards_collected.append(card1)
+        self.cards_collected.append(card2)
+
+    def count_score(self):
+        for card in self.cards_collected:
+            self.score += card.value
+        return self.score
+
+
+
