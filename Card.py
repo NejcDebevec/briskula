524fccecb0442868363ec6f372d0a1310fe5d5a5
Nejc Debevec <nd8380@student.uni-lj.si>
2018-05-21 16:04:34 +0200

    Postavitev projekta

---
+++ Card.py
@@ -0,0 +1,6 @@
+class Card:
+    def __init__(self,card, value, barva):
+        self.card = card
+        self.value = value
+        self.barva = barva
+
---
+++ Deck.py
@@ -0,0 +1,12 @@
+import Card
+
+class Deck:
+    def __init__(self):
+        barve = ["Spade", "Coppe", "Bastoni", "Danari"]
+        karte = ["As", "Trojka", "Kralj", "Kaval", "Fant", "7", "6", "5", "4", "2"]
+        vrednosti = ["11", "10", "4", "3", "2", "0", "0", "0", "0", "0"]
+        self.deck = []
+        for barva in barve:
+            for n in range(len(karte)):
+                self.deck.append(Card(karte[n], vrednosti[n], barva))
+
